d20866efce4a2e2881ff6fe7eef6d9da6a46c22d
KhanhDoHoang <katievincy226@gmail.com>
2020-09-21 11:25:22 -0400

    Added play and background sounds

---
+++ app.js
@@ -0,0 +1,41 @@
+const app = () => {
+    const song = document.querySelector(".song");
+    const play = document.querySelector(".play");
+    const outline = document.querySelector(".moving-outline circle");
+    const video = document.querySelector("vid-container video");
+
+
+    const sounds = document.querySelectorAll(".sound-picker buttons");
+    //Time display
+
+    const timeDisplay = document.querySelector(".time-display");
+    //Get the length of the outline
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+    //Duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+
+    //play sounds
+    play.addEventListener("click", () => {
+        checkPlaying(song);
+    })
+
+    //Create a function
+    const checkPlaying = song => {
+        if (song.paused) {
+            song.play();
+            video.play();
+            play.src = "./svg/pause.svg";
+        } else {
+            song.pause();
+            video.pause();
+            play.src = "./svg/play.svg";
+        }
+    }
+}
+
+app();
